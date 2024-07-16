b13515e030a4841681732472b6c18286e272e09f
Pablo <pncm02@gmail.com>
2024-07-16 17:33:32 +0200

    presentación personajes matedivas

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,15 @@ const Contact = () => {
   return (
     <div>
       <h1>La leyenda de la Puchaina Matediva</h1>
-      <p>Esta página esta dedicada a la yincana que va sobre la leyenda de la puchaina matediva</p>
+      <p>
+        Cuando Jiafei creó el mundo, no dejo sin sitio a las matemáticas. Para ello, creo la convención de las matedivas. Un grupo anónimo compuesto por expertas en 
+        matemáticas que deberían de velar por la paz mundial.
+      </p>
+      <p>
+        Antes de esta aventura, Miguel "el bombas", José "el caminante de la pradera", Alberto "el gymbro", Raúl "el marido deseado con tierras", Angy "la trotamundos" y Alba "la gestora"
+        se encontraban investigando sobre esta organización secreta, sin embargo, hoy parece que todo va a cambiar de rumbo, pues les ha llegado una mochila.
+        (Mochila de Migul, quedan x tiempo para que todo termine, firmado las matedivas PD: hay sitios donde es mejor no entrometerse)
+      </p>
     </div>
   );
 };
