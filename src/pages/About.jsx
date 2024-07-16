8e798faf0da5f378b43e378d417c1c599b2c29ac
Pablo <pncm02@gmail.com>
2024-07-16 17:21:22 +0200

    algunas instrucciones de la yincana

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,10 +4,9 @@ const About = () => {
     return (
         <div className='pag-principales'>
           <h1>Instrucciones</h1>
-          <p>Esto es una pequeña guía de lo que vas a ver a continuación.<br />
-            Tu regalo es una especie de juego de rol en la vida real. <br />
-            Como el juego es en la realidad y la vida es un mundo difícil, el reto lo afrontarás solo. <br />
-            Reglas: </p>
+          <p>Esto es una pequeña guía de lo que vais a ver a continuación.<br />
+             Vuestro regalo es una especie de juego de rol en la vida real. <br />
+             Reglas: </p>
           <p className='extra-margin'>  
             1.	No mirar el código del proyecto. <br />
             2.	El dispositivo desde donde lo empieces será tu centro de operaciones (preferiblemente usa el HP porque es el único donde se ha <br />
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -13,7 +13,13 @@ const Services = () => {
         A partir de aquí vuestro trabajo va a ser demostrar este teorema e ir avanzando por definiciones, lemas y proposiciones que os terminarán llevando al Teorema
         Fundamental de Paco Pico. <br />
       </p>
-      <p>Esperemos que te guste, se ha hecho con mucho cariño y con el fin de que lo disfrutes al máximo.</p>
+      <p>
+        En esta aventura formarán parte seis intrépidos investigadores de matemáticas:
+        Clara investigadora en teoría de grupos sectarios, Julia que esta investigando algo, pero no se ha enterado muy bien el qué, Candela 
+        que se ha adentrado en la teoría de los cuerpos perfectos (los rumores hablan de que tiene que ver algo con su hijo), Irene que es experta en superficies y 
+        cuerpos de la pradera, Nura algebrista enamorada de la geometría y Carlos, que consigue apuntes que no se saben ni que existían.
+      </p>
+      <p>Esperemos que os guste, se ha hecho con mucho cariño y con el fin de que lo disfruteis al máximo.</p>
       <button className='custom-button' onClick={() => alert('Botón personalizado clickeado!')}>
         Siguiente Nivel
       </button>
