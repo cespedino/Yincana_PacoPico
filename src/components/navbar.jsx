0d37fd93703bd1c4c0a6127c5ef21ff8f5664c92
Pablo <pncm02@gmail.com>
2024-07-16 16:49:26 +0200

    navdar se despliega y quita automático

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
 import logo from '../images/logo.png';
 
 const NavigationBar = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleClose = () => {
+    setExpanded(false);
+  };
+
   return (
-    <Navbar className="navbar-custom" expand="lg" fixed='top'>
+    <Navbar className="navbar-custom" expand="lg" fixed="top" expanded={expanded}>
       <Container>
-        <Navbar.Brand as={Link} to="/">
-        <img src={logo} alt="Logo" height="80" />
+        <Navbar.Brand as={Link} to="/" onClick={handleClose}>
+          <img src={logo} alt="Logo" height="80" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">Instrucciones</Nav.Link>
-            <Nav.Link as={Link} to="/services">Teorema de Paco Pico</Nav.Link>
-            <Nav.Link as={Link} to="/contact">La Leyenda de la Puchaina Matediva</Nav.Link>
+            <Nav.Link as={Link} to="/" onClick={handleClose}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/about" onClick={handleClose}>Instrucciones</Nav.Link>
+            <Nav.Link as={Link} to="/services" onClick={handleClose}>Teorema de Paco Pico</Nav.Link>
+            <Nav.Link as={Link} to="/contact" onClick={handleClose}>La Leyenda de la Puchaina Matediva</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -26,3 +36,4 @@ const NavigationBar = () => {
 }
 
 export default NavigationBar;
+
