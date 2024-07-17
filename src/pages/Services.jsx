0601fbdd00a48721cddb829a5a455f6638ba2279
Pablo <pncm02@gmail.com>
2024-07-17 11:22:06 +0200

    fase 1 en teorema de paco pico

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/home.css'
 
 const Services = () => {
+
+  const [inputValue, setInputValue] = useState('');
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    if (e.target.value === 'secreta') { // Reemplaza 'secreta' con la palabra secreta que desees
+      setIsButtonVisible(true);
+    } else {
+      setIsButtonVisible(false);
+    }
+  };
+
+  const handleButtonClick = () => {
+    navigate('/fase1');
+  };
+
   return (
     <div className='pag-principales' id="inicio">
       <h1>El teorema de Paco Pico</h1>
@@ -20,9 +40,17 @@ const Services = () => {
         cuerpos de la pradera, Nura algebrista enamorada de la geometría y Carlos, que consigue apuntes que no se saben ni que existían.
       </p>
       <p>Esperemos que os guste, se ha hecho con mucho cariño y con el fin de que lo disfruteis al máximo.</p>
-      <button className='custom-button' onClick={() => alert('Botón personalizado clickeado!')}>
-        Siguiente Nivel
-      </button>
+      <input
+        type="text"
+        placeholder="Introduce la palabra secreta"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      {isButtonVisible && (
+        <button onClick={handleButtonClick} className="custom-button">
+          Siguiente Página
+        </button>
+      )}
     </div>
   );
 };
