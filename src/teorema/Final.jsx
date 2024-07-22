42e8b670da41b24ce4b90b11f4301a5cb36f155f
Pablo <pncm02@gmail.com>
2024-07-22 09:38:50 +0200

    comienzo fase final

---
+++ src/teorema/Final.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/pages.css';
+
+const Fase12 = () => {
+  return (
+    <div className="page-content">
+      <h1>Corolario Teorema Paco Pico</h1>
+      <p>
+        Sin embargo, es trivial demostrar el siguiente corolario a partir del resultado anterior: <br/>
+        Formando parte de la secta de Paco Pico se recorre un viaje, distinto para cada miembro, con diferentes relaciones para cada elemento. 
+        Unos serán burros, otros un club de fans de Morcillo o incluso algunas matedivas.
+
+        Vuestro regalo no es una yincana, ni una página web, pero se parece a un teorema.
+         
+        Esos pequeños (inmensos) detalles, 
+        Las comidas en el dominos, para luego pasar por delante de vitalia.
+        Las vueltas por la noche de las clases de tarde.
+        La música clásica de Amable antes de clase (y sus aprobados).
+        Las contantes de wilfri y los chuletarios.
+
+        Los comentarios de Clara y Candela sobre One Direction.
+        La mochila de Carlos.
+        O las cenas en el Rosario,
+
+        Simplemente detalles que nos han acompañado y que no vemos por la prisa del día, por el agobio de los exámenes o la vida en general.
+        
+        Vuestro regalo es un recordatorio de que estos momentos que hemos pasado, están pasando (y pasarán), al igual que los matemáticas que se descubren 
+        y no se usan hasta años después, es necesario y suficiente que pase el tiempo para que veáis que al igual que los teoremas, os acompañarán para siempre.
+      </p>
+      <h2>acertijo </h2>
+      <p className='acertijos'>
+        acertijo
+      </p>
+    </div>
+  );
+};
+
+export default Fase12;
