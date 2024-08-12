3ee6c70d1e549b0783bafdb312726dea3fe6fa8b
Pablo <pncm02@gmail.com>
2024-08-12 16:59:41 +0200

    pequeñas mejoras

--- src/pages/Zombie.jsx
+++ src/pages/Zombie.jsx
@@ -6,12 +6,18 @@ const Zombie = () => {
   return (
     <div className='page-content'>
       <h1>La invasión Zombie</h1>
-      <h2>Prueba de entrada</h2>
       <p>
-        
+        Bienvenidos a esta gran aventura, en ella os tendréis que salvar de la invasión zombie e ir afrontantdo los retos que os vayáis encontrando para poder
+        salvaros del virus zombie
+      </p>
+      <h2>Fase preparación</h2>
+      <p>
+        Coge todo lo que creas que te pueda ser útil y cuando estés listo pasa al acertijo de abajo.
       </p>
       <p>Para poder entrar a esta prueba, os espera un reto para saber si sois dignos de afrontarlo:</p>
-      <p className="acertijos">HHHHH</p>
+      <p className="acertijos">
+        Si miras debajo del sofá podrás encontrar las pistas para poder salir de la casa e intentar salvarte
+      </p>
       
       <SecretInput 
         secretWord="a"
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,9 +7,10 @@ const Home = () => {
     <div className="page-content">
       <h1>¡Bienvenidos, valientes aspirantes a la gloria eterna!</h1>
       <p>
-        No es un simple juego, sino una odisea intelectual, un regalo nacido del deseo de probar los límites de vuestra sagacidad y determinación.
-        Imaginad un espacio infinito, donde los axiomas matemáticos se encuentran con los misterios del corazón humano, donde cada pista es una llave y cada acertijo, una puerta hacia un conocimiento ancestral. <br/ >
-        ¡Adelante, valientes! Este es vuestro momento, vuestra oportunidad de brillar con luz propia. Que la sabiduría os guíe y que el coraje sea vuestra bandera. ¡La gloria os espera!
+        Esta es la página principal de la yincana, desde aquí podréis acceder a las instrucciones y a la yincana en si. <br/>
+        Espero que sea de vuestro agrado, ha sido un proceso bastante largo pero ójala lo disfrutéis. 
+        Cuando la terminéis desbloquearéis acceso a la recompensa final. <br/>
+        ¡Adelante! Este es vuestro momento, vuestra oportunidad de pasar un rato divertido.
       </p>
       <p>
         Para empezar, abrir el desplegable en lo alto de la pantalla y leer las instrucciones
