3163f1f3af327f2ec277923504cf6e5e525840da
Pablo <pncm02@gmail.com>
2025-03-18 10:36:44 +0100

    Ampliación hasta fase7

---
+++ src/city/Fase5.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import SecretInput from '../components/SecretInput';
+import '../styles/pages.css';
+
+const Fase5 = () => {
+  return (
+    <div className="page-content">
+      <h1>Alan Turind</h1>
+      <p>
+      Para el enigma del siglo XX, tendrás que descrifrar la máquina que los produce. Para ello tendrás que decodificar la palabra secreta haciendo uso 
+      de la máquina enigma casera. 
+      </p>
+      <h2>título acertijo</h2>
+      <p className='acertijos'>
+        , <br/>
+
+      </p>
+
+      <SecretInput 
+        secretWord="hola"
+        navigateTo="/city/fase6"
+        buttonText="Siguiente Fase"
+        entradaText={"Introduce la solución a este acertijo"}
+      />
+    </div>
+  );
+};
+
+export default Fase5;
--- src/routes/AlvaroRoutes.jsx
+++ src/routes/AlvaroRoutes.jsx
@@ -4,6 +4,9 @@ import Fase1 from '../city/Fase1';
 import Fase2 from '../city/Fase2';
 import Fase3 from '../city/Fase3';
 import Fase4 from '../city/Fase4';
+import Fase5 from '../city/Fase5';
+import Fase6 from '../city/Fase6';
+import Fase7 from '../city/Fase7';
 
 
 const AlvaroRoutes = () => {
@@ -13,6 +16,9 @@ const AlvaroRoutes = () => {
       <Route path="/fase2" element={<Fase2 />} />
       <Route path="/fase3" element={<Fase3 />} />
       <Route path="/fase4" element={<Fase4 />} />
+      <Route path="/fase5" element={<Fase5 />} />
+      <Route path="/fase6" element={<Fase6 />} />
+      <Route path="/fase7" element={<Fase7 />} />
     </Routes>
   );
 };
